98d4fb5461303b464090f996d2f86f5747888831
Elthon Freitas <elthonf@gmail.com>
2022-02-21 17:54:29 -0300

    Revert "Docker on Azure com Container Instances"
    This reverts commit 861a3e8b23143d1bb9d4820646c6e71801b347bb.
    
    # Conflicts:
    #	containeronazure/modelo/nome_arquivo.pkl

---
+++ containeronazure/modelo/predictflask.py
@@ -0,0 +1,71 @@
+import joblib
+import pandas as pd
+import sys
+import json
+import numpy as np
+from flask import Flask, jsonify, request
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
+app = Flask(__name__)
+app.json_encoder = NpEncoder
+global meumodelo
+
+@app.route("/", methods=['GET', 'POST'])
+def call_home(request = request):
+    print(request.values)
+    return "SERVER IS RUNNING!"
+
+def init():
+    print(f"Python version: {sys.version}")
+    #nltk.download("punkt")
+    global meumodelo
+    #sess = onnxruntime.InferenceSession(
+    #    os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model.onnx")
+    #)
+    #model_path = Model.get_model_path("knn")
+    #print("Model Path is  ", model_path)
+    #model = joblib.load(model_path)
+
+    #meumodelo = joblib.load( './nome_arquivo.pkl')
+    meumodelo = joblib.load( './modelo/nome_arquivo.pkl')
+
+@app.route("/run", methods=['POST'])
+def run(request = request):
+    print(request.values)
+
+    json_ = request.json
+    campos = pd.DataFrame(json_)
+
+    if campos.shape[0] == 0:
+        return "Dados de chamada da API estão incorretos.", 400
+
+    for col in modelo01.independentcols:
+        if col not in campos.columns:
+            campos[col] = 0
+    x = campos[modelo01.independentcols]
+
+    prediction = meumodelo.predict(x)
+    try:
+      predict_proba = meumodelo.predict_proba(x)
+    except Exception as ex:
+      predict_proba = None
+
+    ret = json.dumps({'prediction': list(prediction),
+                      'proba': list(predict_proba),
+                      'author': "Elthon Manhas de Freitas"}, cls=NpEncoder)
+
+    return app.response_class(response=ret, mimetype='application/json')
+
+if __name__ == '__main__':
+    init()
+    app.run(port=80, host = '0.0.0.0')
